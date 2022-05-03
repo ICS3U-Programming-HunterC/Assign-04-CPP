d75cb874e375f4abc9f91e683cae2db1d1f74258
Ubuntu <ubuntu@ip-172-31-80-220.ec2.internal>
2022-05-03 02:34:08 +0000

    finsihed dice.cpp

--- dice.cpp
+++ dice.cpp
@@ -1,10 +1,7 @@
 // Copyright (c) 2022 Hunter Connolly All rights reserved
 // Created by: Hunter Connolly
 // Created on: April 28, 2022
-// This program asks the user for the number of numbers to add together.
-// It then uses a while loop to ask the user for the number to add
-// and If it is a whole number, add it to the current sum. Otherwise,
-// don't and continue the loop then display the numbers added and the final sum
+// This program rolls a dice and if the roll a double they win
 
 #include <iostream>
 #include <string>
@@ -15,45 +12,47 @@ int main() {
     // declare variables
     std::string rollAgain;
     int random1, random2;
-    
+
     // initialize min and max for random number, and counter
     int min = 1;
     int max = 6;
     int counter = 0;
-    
+
     rollAgain = "yes";
-    
-    while (rollAgain == "yes" or rollAgain == "y") {
+
+    while (rollAgain == "yes" || rollAgain == "y") {
         counter = counter + 1;
-        // generate 2 random numbers 
+
+        // generate 2 random numbers
         // first random number
         int random1;
 
         std::random_device rseed;
 
-        std::mt19937 rgen(rseed()); // mersenne_twister
+        std::mt19937 rgen(rseed());
 
-        std::uniform_int_distribution<int> idist(1, 6); // [1,6]
+        std::uniform_int_distribution<int> idist(min, max);  //  [1,6]
 
         random1 = idist(rgen);
-        
+
         // 2nd random number
         int random2;
 
         std::random_device rseed2;
 
-        std::mt19937 rgen2(rseed()); // mersenne_twister
+        std::mt19937 rgen2(rseed());
 
-        std::uniform_int_distribution<int> idist2(1, 6); // [1,6]
+        std::uniform_int_distribution<int> idist2(min, max);  //  [1,6]
 
         random2 = idist(rgen);
-        
-        std::cout << "Rolling the dices...\n";
+
+        // display the dice rolled
+        std::cout << "Rolling the dice...\n";
         std::cout << "The values are....\n";
         std::cout << random1 << ", " << random2 << "\n";
         std::cout << "\n";
-        
-        
+
+
         // if the dice are the same, then it will roll doubles and the
         // program will end and tell them how many attempts it took
         if (random1 == random2) {
@@ -61,17 +60,15 @@ int main() {
             std::cout << "It took " << counter << " tries to roll doubles\n";
             break;
         }
-        
+
         // ask the user if they want to roll again
         std::cout << "Roll the dices again?: ";
         std::cin >> rollAgain;
-        
+
         // if the user enters no the program will end
-        if (rollAgain == "no" or rollAgain == "n") {
+        if (rollAgain == "no" || rollAgain == "n") {
             std::cout << "Thanks for playing!!";
             break;
         }
-        
     }
-
 }
